662a8517660610f088b93bad93b957e5879fade1
lilieth <liliethramirez91@gmail.com>
2024-09-28 18:38:42 -0300

    Deploy2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,16 @@
-// src/app.tsx
-import '@/styles/globals.css'; // Importa Tailwind CSS
+import '@/styles/globals.css'; // Importar Tailwind CSS
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from '@/styles/theme'; // Ajusta la ruta si lo colocaste en `styles`
-import Footer from '@/Components/Footer';
-import NavBar from '@/Components/NavBar';
+import theme from '@/styles/theme'; // Importar tema de Chakra UI, si lo tienes
+import NavBar from '@/components/NavBar';
+import Footer from '@/components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <NavBar/>
+      <NavBar /> {/* Este puede usar Tailwind */}
       <Component {...pageProps} />
-      <Footer/>
+      <Footer /> {/* Este también puede usar Tailwind */}
     </ChakraProvider>
-   
   );
 }
