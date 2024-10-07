9271c8c52ffd9332bf2f630814903184ebe450d4
lilieth <liliethramirez91@gmail.com>
2024-10-07 16:23:48 -0300

    add form

--- src/Components/AboutCard.tsx
+++ src/Components/AboutCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Heading, Text, List, ListItem, Icon, SimpleGrid, Flex } from '@chakra-ui/react';
+import { Box, Heading, Text, List, ListItem, Icon, SimpleGrid, Flex, Image } from '@chakra-ui/react';
 import { ImPointRight } from 'react-icons/im';
 import {
   DiJavascript1, DiReact, DiNodejs, DiMongodb, DiGit, DiPython, DiJava
@@ -8,92 +8,81 @@ import { SiNextdotjs, SiPostgresql, SiFirebase, SiChakraui } from 'react-icons/s
 
 const AboutCard: React.FC = () => {
   return (
-    <Box p={6} border="1px solid #e0e0e0" boxShadow="md" rounded="md">
-      {/* Sección de presentación */}
-      <Heading as="h2" size="lg" mb={4} textAlign="justify">
-        Hola, soy <span className="purple">Lilieth Chacon</span> de <span className="purple">Venezuela</span>.
-      </Heading>
-      <Text fontSize="md" textAlign="justify">
-        Actualmente soy desarrolladora de software. Me apasiona el desarrollo backend con Node.js, Express y Nest.js.
-      </Text>
-      
-      <Text fontSize="md" textAlign="justify" mt={4}>
-        ¡Además de programar, disfruto hacer estas actividades!
-      </Text>
-      <List spacing={2} mt={4}>
-        <ListItem>
-          <Icon as={ImPointRight} /> Aprender nuevas tecnologías
-        </ListItem>
-        <ListItem>
-          <Icon as={ImPointRight} /> Leer libros
-        </ListItem>
-        <ListItem>
-          <Icon as={ImPointRight} /> Disfrutar de la naturaleza
-        </ListItem>
-      </List>
+    <Box p={6}>
+      <Flex direction={['column', 'column', 'row']} align="center" justify="space-between">
+        {/* Sección de presentación con imagen */}
+        <Box flex="1">
+          <Heading as="h2" size="lg" mb={4} textAlign="justify">
+            Hola, soy <span className="purple">Lilieth Chacon</span> de <span className="purple">Venezuela</span>.
+          </Heading>
+          <Text fontSize="md" textAlign="justify">
+            Actualmente soy desarrolladora de software. Me apasiona el desarrollo backend con Node.js, Express y Nest.js.
+          </Text>
+          
+          <Text fontSize="md" textAlign="justify" mt={4}>
+            ¡Además de programar, disfruto hacer estas actividades!
+          </Text>
+          <List spacing={2} mt={4}>
+            <ListItem>
+              <Icon as={ImPointRight} /> Aprender nuevas tecnologías
+            </ListItem>
+            <ListItem>
+              <Icon as={ImPointRight} /> Leer libros
+            </ListItem>
+            <ListItem>
+              <Icon as={ImPointRight} /> Disfrutar de la naturaleza
+            </ListItem>
+            <ListItem>
+              <Icon as={ImPointRight} /> Bailar 
+            </ListItem>
+          </List>
 
-      {/* Cita inspiradora */}
-      <Text fontSize="md" color="purple.400" mt={6}>
-        "Esfuérzate por crear cosas que marquen la diferencia."
-      </Text>
-      <Text as="footer" textAlign="right" mt={2}>
-        - Lilieth
-      </Text>
+          {/* Cita inspiradora */}
+          <Text fontSize="md" color="purple.400" mt={6}>
+            "Esfuérzate por crear cosas que marquen la diferencia."
+          </Text>
+          <Text as="footer" textAlign="right" mt={2}>
+            - Lilieth
+          </Text>
+        </Box>
+
+        {/* Imagen ilustrativa */}
+        <Box flex="-1" mt={[6, 6, 0]} textAlign="center">
+          <Image
+            src="/womman.png" // Ruta a la imagen en tu carpeta public
+            alt="Ilustración"
+            boxSize="250px"
+            objectFit="contain"
+          />
+        </Box>
+      </Flex>
 
       {/* Sección de habilidades profesionales */}
-      <Heading as="h3" size="md" mt={10} mb={6}>
-        Skillset Profesional
+      <Heading as="h3" size="md" mt={10} mb={6} textAlign="center">
+        Conjunto de habilidades profesionales
       </Heading>
-      <SimpleGrid columns={[2, 3, 6]} spacing={6}>
-        <Flex justify="center" align="center">
-          <Icon as={DiJavascript1} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={DiReact} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={SiNextdotjs} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={DiNodejs} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={DiMongodb} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={DiGit} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={SiPostgresql} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={SiFirebase} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={DiPython} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={DiJava} boxSize={10} />
-        </Flex>
+      <SimpleGrid columns={[2, 3, 6]} spacing={8} mt={4}>
+        <Icon as={DiJavascript1} boxSize={10} />
+        <Icon as={DiReact} boxSize={10} />
+        <Icon as={SiNextdotjs} boxSize={10} />
+        <Icon as={DiNodejs} boxSize={10} />
+        <Icon as={DiMongodb} boxSize={10} />
+        <Icon as={DiGit} boxSize={10} />
+        <Icon as={SiPostgresql} boxSize={10} />
+        <Icon as={SiFirebase} boxSize={10} />
+        <Icon as={DiPython} boxSize={10} />
+        <Icon as={DiJava} boxSize={10} />
       </SimpleGrid>
 
       {/* Sección de herramientas */}
-      <Heading as="h3" size="md" mt={10} mb={6}>
+      <Heading as="h3" size="md" mt={10} mb={6} textAlign="center">
         Herramientas que utilizo
       </Heading>
-      <SimpleGrid columns={[2, 3, 5]} spacing={6}>
-        <Flex justify="center" align="center">
-          <Icon as={SiChakraui} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={SiPostgresql} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={SiFirebase} boxSize={10} />
-        </Flex>
-        <Flex justify="center" align="center">
-          <Icon as={SiNextdotjs} boxSize={10} />
-        </Flex>
+      <SimpleGrid columns={[2, 3, 5]} spacing={8} mt={4}>
+        <Icon as={SiChakraui} boxSize={10} />
+        <Icon as={SiPostgresql} boxSize={10} />
+        <Icon as={SiFirebase} boxSize={10} />
+        <Icon as={SiNextdotjs} boxSize={10} />
       </SimpleGrid>
     </Box>
   );
--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -2,6 +2,24 @@ import React from 'react';
 import { Box, Heading, Text, FormControl, FormLabel, Input, Textarea, Button } from '@chakra-ui/react';
 
 function Contact() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Previene la acción por defecto del formulario
+
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const message = formData.get('message');
+
+    // Crear el enlace de mailto
+    const mailtoLink = `mailto:liliethkatherine.21@gmail.com?subject=Consulta de ${name}&body=Nombre: ${name}%0D%0ACorreo: ${email}%0D%0AMensaje: ${message}`;
+
+    // Redirigir al usuario a su cliente de correo
+    window.location.href = mailtoLink;
+
+    // Resetea el formulario
+    e.currentTarget.reset();
+  };
+
   return (
     <Box p={4}>
       <Heading as="h2" mb={6} textAlign="center">
@@ -10,18 +28,18 @@ function Contact() {
       <Text mb={4} textAlign="center">
         Si tienes alguna pregunta o deseas agendar una consulta, no dudes en contactarme a través del siguiente formulario.
       </Text>
-      <Box maxW="lg" mx="auto">
+      <Box maxW="lg" mx="auto" as="form" onSubmit={handleSubmit}>
         <FormControl id="name" mb={4} isRequired>
           <FormLabel>Nombre</FormLabel>
-          <Input type="text" placeholder="Tu nombre" />
+          <Input type="text" name="name" placeholder="Tu nombre" required />
         </FormControl>
         <FormControl id="email" mb={4} isRequired>
           <FormLabel>Correo Electrónico</FormLabel>
-          <Input type="email" placeholder="Tu correo" />
+          <Input type="email" name="email" placeholder="Tu correo" required />
         </FormControl>
         <FormControl id="message" mb={4} isRequired>
           <FormLabel>Mensaje</FormLabel>
-          <Textarea placeholder="Escribe tu mensaje aquí" />
+          <Textarea name="message" placeholder="Escribe tu mensaje aquí" required />
         </FormControl>
         <Button colorScheme="blue" type="submit" width="full">
           Enviar Mensaje
